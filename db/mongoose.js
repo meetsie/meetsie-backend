563656718898e360409872dcfab4f235da073183
thawalk <tharunana@gmail.com>
2021-01-27 23:11:50 +0800

    edit_api_call

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose')
 
-
-
-
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
--- routers/meeting.js
+++ routers/meeting.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Meeting = require('../models/meeting')
 const router = new express.Router()
 
-router.post('/event', async (req, res) => {
+router.post('/post_meeting', async (req, res) => {
     const meeting = new Meeting({
         ...req.body
     })
@@ -16,6 +16,23 @@ router.post('/event', async (req, res) => {
     }
 
 })
+
+router.get('/get_meeting/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const meeting = await meeting.findOne({_id})
+        if (!meeting) {
+            return res.status(404).send();
+        }
+        res.send(meeting)
+    }
+    catch (e) {
+        res.status(500).send(e)
+    }
+
+})
+
 
 
 module.exports = router;
